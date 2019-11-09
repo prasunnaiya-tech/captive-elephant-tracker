e93e382459c56c365a4d683c819c80677186be2d
Prasun Naiya <prasun.naiya@cognizant.com>
2019-11-09 11:48:28 +0530

    Running version

--- src/main/java/org/wild/animal/captive/CaptiveElephantApplication.java
+++ src/main/java/org/wild/animal/captive/CaptiveElephantApplication.java
@@ -1,9 +1,11 @@
 package org.wild.animal.captive;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class CaptiveElephantApplication {
 
 	public static void main(String[] args) {
