07d485d380f62bc06161d47ba67c4e032e728156
Prasun Naiya <prasun.naiya@cognizant.com>
2019-11-09 11:29:52 +0530

    Initial Structure

---
+++ src/main/java/org/wild/animal/captive/CaptiveElephantApplication.java
@@ -0,0 +1,13 @@
+package org.wild.animal.captive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptiveElephantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaptiveElephantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wild/animal/captive/CaptiveElephantApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wild.animal.captive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaptiveElephantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
